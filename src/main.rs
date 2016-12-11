1c9c0d8082086249ba70d84ff24cb35b1655a79a
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2016-12-12 00:39:52 +0900

    ok

--- src/main.rs
+++ src/main.rs
@@ -38,23 +38,9 @@ impl Env {
     fn trace(&self, ray: geometry::Ray, depth: i32) -> i32 {
         if depth <= 0 { return 0; }
 
-        let hit = self.hit(ray);
-
-        match hit {
+        match self.hit(ray) {
             None      => 0,
             Some(hit) => {
-
-                // println!("{}", hit.t);
-
-                // match hit.material {
-                //     obj::Material::Diffuse => {
-                //         50
-                //     },
-                //     obj::Material::Specular => {
-                //         100
-                //     }
-                // }
-
                 match hit.material {
                     obj::Material::Diffuse  => {
                         let power_directional = geometry::dot(hit.normal, self.directional_light).max(0.0);
@@ -64,21 +50,15 @@ impl Env {
                             origin: hit.point,
                             unit_dir: shadow.normalize()
                         };
-                        // println!("ray_to_light_dir = {}\nhit.pint = {}\nhit.normal = {}", ray_to_light_dir, hit.point, hit.normal);
-                        let power_positional = match self.hit(shadow_ray) {
+                        let power_positional = (match self.hit(shadow_ray) {
                             Some(h) => if h.t > shadow.size() {
-                                // println!("{}, {}", hit.t, ray_to_light_dir.size());
-                                // geometry::dot(hit.normal, -self.positional_light.unit_dir).max(0.0)
-                                // println!("{}, {}", hit.normal, ray_to_light_dir);
                                 geometry::dot(hit.normal, shadow_ray.unit_dir).max(0.0)
-                            } else {
-                                0.0
-                            },
-                            None      => {
+                            } else { 0.0 }, // 障害物がある場合
+                            None => {
                                 geometry::dot(hit.normal, shadow_ray.unit_dir).max(0.0)
                             }
-                        };
-                        (power_directional * 127.0 + power_positional * 127.0) as i32
+                        } / (shadow.size() + 0.001).powf(1.5) * 150.0).min(1.0).max(0.0);
+                        (power_directional * 0.0 + power_positional * 255.0) as i32 // 点光源だけ
                     },
                     obj::Material::Specular => {
                         let dir = ray.unit_dir + 2.0 * geometry::dot(-ray.unit_dir, hit.normal) * hit.normal;
@@ -100,44 +80,45 @@ impl Env {
 
 fn main() {
     let env = Env {
-        camera: geometry::Vec3 { x: 0.0, y: 0.0, z: -10.0 },
-        directional_light: geometry::Vec3 { x: -1.0, y: -2.0, z: -0.9 }.normalize(),
+        camera: geometry::Vec3 { x: 0.0, y: 0.0, z: -50.0 },
+        directional_light: geometry::Vec3 { x: -1.0, y: 1.0, z: 0.0 }.normalize(),
         positional_light: geometry::Ray {
-            origin: geometry::Vec3 { x: 0.0, y: 0.0, z: 0.0 },
-            unit_dir: geometry::Vec3 { x: -1.2, y: 1.5, z: 1.0 }.normalize()
+            origin: geometry::Vec3 { x: 0.0, y: -24.0, z: -5.0 },
+            unit_dir: geometry::Vec3 { x: -1.2, y: 1.5, z: 1.0 }.normalize() // unused
         },
         spheres: vec![
-            obj::sphere_new(7.0, 7.0, 5.0, 7.0, obj::Material::Diffuse),
-            obj::sphere_new(5.0, -10.0, 5.0, 7.0, obj::Material::Specular),
-            obj::sphere_new(-10.0, -10.0, 5.0, 4.0, obj::Material::Specular),
+            obj::sphere_new( 12.0,   0.0, -2.0, 2.0, obj::Material::Diffuse),
+            obj::sphere_new( -4.0,  13.0,  9.0, 2.0, obj::Material::Diffuse),
+            obj::sphere_new( 16.0,  -5.0, -2.0, 2.0, obj::Material::Diffuse),
+            obj::sphere_new(  0.0,   4.0,  8.0, 2.0, obj::Material::Diffuse),
+            obj::sphere_new(-13.0, -13.0, -4.0, 3.0, obj::Material::Diffuse),
+            obj::sphere_new(-10.0, -16.0,  3.0, 2.0, obj::Material::Diffuse),
+            obj::sphere_new(-14.0,  18.0,  5.0, 8.0, obj::Material::Specular), // left sphere
+            obj::sphere_new( 13.0,  18.0,  0.0, 8.0, obj::Material::Specular), // right sphere
         ],
         planes: vec![
-            obj::plane_new(0.0, 0.0, -1.0, -20.0, obj::Material::Diffuse),
-            obj::plane_new(-1.0, 0.0, 0.0, -40.0, obj::Material::Diffuse),
-            obj::plane_new(1.0, 0.0, 0.0, -40.0, obj::Material::Diffuse),
-            obj::plane_new(0.0, -1.0, 0.0, -40.0, obj::Material::Diffuse),
-            obj::plane_new(0.0, 1.0, 0.0, -40.0, obj::Material::Diffuse),
+            obj::plane_new( 0.0,  0.0, -1.0, -20.0, obj::Material::Diffuse), // bottom
+            obj::plane_new(-1.0,  0.0,  0.0, -25.0, obj::Material::Diffuse), // right wall
+            obj::plane_new( 1.0,  0.0,  0.0, -25.0, obj::Material::Diffuse), // left wall
+            obj::plane_new( 0.0, -1.0,  0.0, -25.0, obj::Material::Diffuse), // floor
+            obj::plane_new( 0.0,  1.0,  0.0, -25.0, obj::Material::Diffuse), // ceil
         ]
     };
 
-    let left = -200;
-    let right = 200;
-    let bottom = -200;
-    let top = 200;
+    let left = -300;
+    let right = 300;
+    let bottom = -300;
+    let top = 300;
 
     println!("P2");
     println!("{} {}", right - left, top - bottom);
     println!("255");
 
-    // (-100, -100, 0) .. (99, 99, 0)
     for y in bottom..top {
         for x in left..right {
-            let point = geometry::Vec3 { x: 0.1 * x as f64,
-                                         y: 0.1 * y as f64,
-                                         z: 0.0 };
-            // println!("{}", point);
+            let point = geometry::Vec3 { x: 0.08 * x as f64, y: 0.08 * y as f64, z: -10.0 };
             let ray = env.shot(point);
-            let power = env.trace(ray, 10);
+            let power = env.trace(ray, 5);
 
             print!("{}", power);
 
